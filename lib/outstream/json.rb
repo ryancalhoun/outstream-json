a73de78edffbcf2eb8fa53950043582564b047b2
Ryan Calhoun <ryanjamescalhoun@gmail.com>
2016-02-13 10:40:15 -0600

    changed generate to create; small refactor

--- lib/outstream/json.rb
+++ lib/outstream/json.rb
@@ -2,22 +2,13 @@ require 'json'
 
 module Outstream
   class Json
-    def self.generate(&body_block)
+    def self.create(&body_block)
       new body_block
     end
 
     def each(&out_block)
       e = Enumerator.new {|yielder|
-        @yielder = yielder
-        write_object {
-          receiver = Receiver.new self
-          if @body_block.arity == 1
-            @body_block[receiver]
-          else
-            (class << receiver; self; end).send(:define_method, :__call_block, &@body_block)
-            receiver.__call_block
-          end
-        }
+        Collector.new(yielder).collect &@body_block
       }
 
       out_block ? e.each(&out_block) : e
@@ -27,65 +18,80 @@ module Outstream
       "".tap {|s| each {|str| s.concat str } }
     end
 
-    def write(objs)
-      objs.each {|key,val|
-        write_key key
-        write_value val
-      }
-    end
-
     private
+
     def initialize(body_block)
-      @count = [0]
       @body_block = body_block
     end
 
-    def print(str)
-      @yielder << str
-    end
-    def write_key(key)
-      print "," if @count.last > 0
-      print key.to_json
-      print ":"
-      @count[@count.size-1] += 1
-    end
-    def write_object
-      print "{"
-      @count.push 0
-      result = yield
-      @count.pop
-      print "}"
-
-      result
-    end
-    def write_array(a)
-      print "["
-      a.enum_for(:each).each_with_index {|v,i|
-        print "," if i > 0
-        write_value v
-      }
-      print "]"
-    end
-    def write_value(value)
-      if value.respond_to?:each_pair
-        write_object { write value }
-      elsif value.respond_to?:each
-        write_array value
-      elsif value.respond_to?:call
-        write_value value.call
-      else
-        print value.to_json
-      end
-    end
-
     class Receiver
-      def initialize(json)
-        @_json = json
+      def initialize(collector)
+        @_collector = collector
       end
       def write(objs)
-        @_json.write objs
+        @_collector.write objs
       end
     end
 
+    class Collector
+      def initialize(yielder)
+        @yielder = yielder
+        @count = [0]
+      end
+      def collect(&block)
+        write_object {
+          receiver = Receiver.new self
+          if block.arity == 1
+            block[receiver]
+          else
+            (class << receiver; self; end).send(:define_method, :__call_block, &block)
+            receiver.__call_block
+          end
+        }
+      end
+      def print(str)
+        @yielder << str
+      end
+      def write(objs)
+        objs.each {|key,val|
+          write_key key
+          write_value val
+        }
+      end
+      def write_key(key)
+        print "," if @count.last > 0
+        print key.to_json
+        print ":"
+        @count[@count.size-1] += 1
+      end
+      def write_object
+        print "{"
+        @count.push 0
+        result = yield
+        @count.pop
+        print "}"
+
+        result
+      end
+      def write_array(a)
+        print "["
+        a.enum_for(:each).each_with_index {|v,i|
+          print "," if i > 0
+          write_value v
+        }
+        print "]"
+      end
+      def write_value(value)
+        if value.respond_to?:each_pair
+          write_object { write value }
+        elsif value.respond_to?:each
+          write_array value
+        elsif value.respond_to?:call
+          write_value value.call
+        else
+          print value.to_json
+        end
+      end
+    end
   end
 end
--- test/test_json.rb
+++ test/test_json.rb
@@ -4,64 +4,64 @@ require 'outstream-json'
 class TestJson < Test::Unit::TestCase
 
   def testScalar
-    out = Outstream::Json.generate {
+    out = Outstream::Json.create {
       write "foo" => "bar"
     }
     assert_equal '{"foo":"bar"}', out.to_s
   end
   def testScalarWithBlockParam
-    out = Outstream::Json.generate {|json|
+    out = Outstream::Json.create {|json|
       json.write "foo" => "bar"
     }
     assert_equal '{"foo":"bar"}', out.to_s
   end
   def testMultipleScalars
-    out = Outstream::Json.generate {
+    out = Outstream::Json.create {
       write "foo" => "bar", "wow" => true
     }
 
     assert_equal '{"foo":"bar","wow":true}', out.to_s
   end
   def testArray
-    out = Outstream::Json.generate {
+    out = Outstream::Json.create {
       write "foo" => [1,2,3]
     }
 
     assert_equal '{"foo":[1,2,3]}', out.to_s
   end
   def testHash
-    out = Outstream::Json.generate {
+    out = Outstream::Json.create {
       write "foo" => {a: 1, b: 2}
     }
     assert_equal '{"foo":{"a":1,"b":2}}', out.to_s
   end
 
   def testProc
-    out = Outstream::Json.generate {
+    out = Outstream::Json.create {
       write "foo" => lambda { "bar" }
     }
     assert_equal '{"foo":"bar"}', out.to_s
   end
   def testProcArray
-    out = Outstream::Json.generate {
+    out = Outstream::Json.create {
       write "foo" => lambda { [1,2,3] }
     }
     assert_equal '{"foo":[1,2,3]}', out.to_s
   end
   def testProcArrayEach
-    out = Outstream::Json.generate {
+    out = Outstream::Json.create {
       write "foo" => lambda { [1,2,3].each }
     }
     assert_equal '{"foo":[1,2,3]}', out.to_s
   end
   def testProcHash
-    out = Outstream::Json.generate {
+    out = Outstream::Json.create {
       write "foo" => lambda { { a: 1, b: 2 } }
     }
     assert_equal '{"foo":{"a":1,"b":2}}', out.to_s
   end
   def testProcHashOfProc
-    out = Outstream::Json.generate {
+    out = Outstream::Json.create {
       write "foo" => lambda { { a: lambda { 1 }, b: lambda { 2 } } }
     }
     assert_equal '{"foo":{"a":1,"b":2}}', out.to_s
@@ -69,7 +69,7 @@ class TestJson < Test::Unit::TestCase
 
   def testLocalVariableScope
     x = "bar"
-    out = Outstream::Json.generate {
+    out = Outstream::Json.create {
       y = "cool"
       write "foo" => x, "wow" => y
     }
@@ -78,7 +78,7 @@ class TestJson < Test::Unit::TestCase
 
   def testEnumerator
     x = "fun"
-    out = Outstream::Json.generate {
+    out = Outstream::Json.create {
       write "foo" => "bar", "wow" => x
     }
     e = out.each
